6c3fbbcc63ebd1c72d4f654792cf92a774eb7f4a
Zabee52 <zabeee52@gmail.com>
2022-10-08 23:50:19 +0900

    gRPC 붙여보는 중...

--- for-daz-service/cmd/for-daz-service/main.go
+++ for-daz-service/cmd/for-daz-service/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 func main() {
@@ -14,4 +18,11 @@ func main() {
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
